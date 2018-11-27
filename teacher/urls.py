bb98ee5c8d86541022721ef464ccec5fe7d896c9
orvi2014 <das.orvi@yahoo.com>
2018-11-28 01:26:09 +0600

    view page complete

--- teacher/urls.py
+++ teacher/urls.py
@@ -4,5 +4,5 @@ from teacher.views import TeacherView
 
 urlpatterns = [
     path('addteacher/', TeacherView.as_view(), name='teacher'),
-    #path('viewteacher/', TeacherView.as_view(), name='teacher'),
+    path('viewteacher/', TeacherView.as_view(template_name= 'view_teacher.html'), name='teacher'),
 ]
--- teacher/views.py
+++ teacher/views.py
@@ -3,16 +3,16 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.views.generic import TemplateView
 from teacher.forms import AddTeacherForm
 from teacher.models import Teachers
+from teacher.models import Teachers
 
 # Create your views here.
 class TeacherView(TemplateView):
      template_name = 'add_teacher.html'
-     def teacher_dashboard(self, request):
-         teacher_results = Teachers.object.all()
-         print(teacher_results)
-         return render(request, 'view_teacher.html', teacher_results)
      def get(self,request):
-         return render(request,'add_teacher.html')
+         model = Teachers
+         teacher_results = Teachers.objects.all()
+         print(teacher_results)
+         return render(request,'view_teacher.html',{'teacher_results': teacher_results})
      def post(self, request):
          form = AddTeacherForm(request.POST)
          if form.is_valid():
@@ -25,3 +25,9 @@ class TeacherView(TemplateView):
          else:
              form = AddTeacherForm()
          return render(request,'add_teacher.html')
+
+# teacher dashboard create
+def teacher_dashboard(self, request):
+    teacher_results = Teachers.object.all()
+    print(teacher_results)
+    return render(request, 'view_teacher.html', teacher_results)
